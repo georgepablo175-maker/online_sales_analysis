e54e4afc794cdc9970c2c2f0a507cd74f045ec8d
George <georgepablo175@gmail.com>
2026-03-31 20:41:30 +0300

    Updated product data and removed inventory display

--- main.py
+++ main.py
@@ -5,17 +5,10 @@ from product_manager import ProductManager
 manager = ProductManager
 
 # adaug produse
-p1 = Product("Telefon", 2000, 3)
-p2 = Product("Laptop", 3500, 2)
-p3 = Product("Casti", 300, 5)
+p1 = Product("Telefon Samsung", 2500, 2)
+p2 = Product("Laptop Gaming", 5000, 1)
+p3 = Product("Mouse", 150, 4)
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
-
-# afisez produse
-print("\nLista produse:")
-manager.display_all_products()
-
-# afisam valoare totala
-manager.total_value()
